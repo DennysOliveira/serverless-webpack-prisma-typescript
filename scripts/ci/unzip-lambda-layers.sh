b9c4c08d5ef869b25a208f77f1e4389acf25dbac
Dennys Oliveira <me@dennys.dev>
2023-01-01 21:30:41 -0300

    remove src

--- scripts/ci/unzip-lambda-layers.sh
+++ scripts/ci/unzip-lambda-layers.sh
@@ -1,16 +1,34 @@
 function unzip_lambda_layers(){
   # Unzip and remove the tar.gz files
+  # layers will be at ./build/layers/layer_name
+
   echo "Unzipping lambda layers..."
+
   for layer in $(ls build/layers); do
     echo "Unzipping $layer"
-    pushd layers/$layer
+    pushd build/layers/$layer
     tar -xzf nodejs.tar.gz
     popd
 
-    # echo "Removing $layer.tar.gz"
-    # rm layers/$layer/nodejs.tar.gz    
+    echo "Removing $layer.tar.gz"
+    rm build/layers/$layer/nodejs.tar.gz    
   done
-  
+
   echo "Done"
+
+  
+
+  # echo "Unzipping lambda layers..."
+  # for layer in $(ls build/layers); do
+  #   echo "Unzipping $layer"
+  #   pushd layers/$layer
+  #   tar -xzf nodejs.tar.gz
+  #   popd
+
+  #   # echo "Removing $layer.tar.gz"
+  #   # rm layers/$layer/nodejs.tar.gz    
+  # done
+  
+  # echo "Done"
 }
 unzip_lambda_layers
