7257bb2c705ab494c80a138c9e3395a61654d009
Dennys Oliveira <me@dennys.dev>
2023-01-01 21:01:07 -0300

    x

--- scripts/ci/unzip-lambda-layers.sh
+++ scripts/ci/unzip-lambda-layers.sh
@@ -1,7 +1,7 @@
 function unzip_lambda_layers(){
   # Unzip and remove the tar.gz files
   echo "Unzipping lambda layers..."
-  for layer in $(ls layers); do
+  for layer in $(ls build/layers); do
     echo "Unzipping $layer"
     pushd layers/$layer
     tar -xzf nodejs.tar.gz
